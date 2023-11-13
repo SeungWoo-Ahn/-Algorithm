e5c11623ccbc53cdd4e0ed00730f810b15ad684d
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-11-13 19:03:57 +0900

    카카오2021 인턴1번

--- programmers/Kakao/Kakao2021Intern_1.java
+++ programmers/Kakao/Kakao2021Intern_1.java
@@ -10,30 +10,31 @@ public class Kakao2021Intern_1 {
 	static List<String> wordArray = new ArrayList<>(Arrays.asList(words));
 	static StringBuilder sb = new StringBuilder();
 	
-//	static int solution(String s) {
-//		String tempStr = "";
-//		char[] charArr = s.toCharArray();
-//		for(int i=0; i<charArr.length; i++) {
-//			if(charArr[i] >= '0' && charArr[i] <= '9') {
-//				sb.append(charArr[i]);
-//			} else {
-//				tempStr += charArr[i];
-//				if(wordArray.contains(tempStr)) {
-//					sb.append(wordArray.indexOf(tempStr));
-//					tempStr = "";
-//				}
-//			}
-//		}
-//		return Integer.valueOf(sb.toString());
-//	}
-	
 	static int solution(String s) {
-        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i = 0; i < strArr.length; i++) {
-            s = s.replaceAll(strArr[i], Integer.toString(i));
-        }
-        return Integer.parseInt(s);
-    }
+		String tempStr = "";
+		char[] charArr = s.toCharArray();
+		for(int i=0; i<charArr.length; i++) {
+			if(charArr[i] >= '0' && charArr[i] <= '9') {
+				sb.append(charArr[i]);
+			} else {
+				tempStr += charArr[i];
+				if(wordArray.contains(tempStr)) {
+					sb.append(wordArray.indexOf(tempStr));
+					tempStr = "";
+				}
+			}
+		}
+		return Integer.valueOf(sb.toString());
+	}
+	
+//	(1µî Á¤´ä)
+//	static int solution(String s) {
+//        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+//        for(int i = 0; i < strArr.length; i++) {
+//            s = s.replaceAll(strArr[i], Integer.toString(i));
+//        }
+//        return Integer.parseInt(s);
+//    }
 
 	public static void main(String[] args) {
 		System.out.println(solution("one4seveneight"));
