f49490e6e78c3c88ebdbe97a3f6a385402f71da9
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-11-13 19:01:55 +0900

    카카오2021인터 1번

---
+++ programmers/Kakao/Kakao2021Intern_1.java
@@ -0,0 +1,42 @@
+package programmers.Kakao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kakao2021Intern_1 {
+	
+	static String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	static List<String> wordArray = new ArrayList<>(Arrays.asList(words));
+	static StringBuilder sb = new StringBuilder();
+	
+//	static int solution(String s) {
+//		String tempStr = "";
+//		char[] charArr = s.toCharArray();
+//		for(int i=0; i<charArr.length; i++) {
+//			if(charArr[i] >= '0' && charArr[i] <= '9') {
+//				sb.append(charArr[i]);
+//			} else {
+//				tempStr += charArr[i];
+//				if(wordArray.contains(tempStr)) {
+//					sb.append(wordArray.indexOf(tempStr));
+//					tempStr = "";
+//				}
+//			}
+//		}
+//		return Integer.valueOf(sb.toString());
+//	}
+	
+	static int solution(String s) {
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+
+	public static void main(String[] args) {
+		System.out.println(solution("one4seveneight"));
+	}
+
+}
